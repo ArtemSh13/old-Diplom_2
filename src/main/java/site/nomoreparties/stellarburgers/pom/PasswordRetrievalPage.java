827632578c41dea6530a8a293ae401dcec555b31
ArtemSh13 <mozg1303@ya.ru>
2022-10-23 16:34:54 +0300

    Add Password Retrieval page to POM

---
+++ src/main/java/site/nomoreparties/stellarburgers/pom/PasswordRetrievalPage.java
@@ -0,0 +1,20 @@
+package site.nomoreparties.stellarburgers.pom;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PasswordRetrievalPage {
+    public final static String PASSWORD_RETRIEVAL_PAGE_URL = ConstructorPage.MAIN_PAGE_URL + "/forgot-password";
+
+    private SelenideElement passwordRetrievalLabel = $(byXpath(".//h2[text()='Восстановление пароля']"));
+
+    private SelenideElement emailField = $(byXpath(".//input[@class='text input__textfield text_type_main-default']"));
+
+    private SelenideElement retrieveButton = $(byXpath(".//button[text()='Восстановить']"));
+
+    private SelenideElement rememberPasswordLabel = $(byXpath(".//p[@class='undefined text text_type_main-default text_color_inactive mb-4']"));
+
+    private SelenideElement signInLink = $(byXpath(".//a[text()='Войти']"));
+}
